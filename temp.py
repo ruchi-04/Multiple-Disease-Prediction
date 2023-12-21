ddf7c17aa66418dcb44d1bd06f341af85df17f1d
ruchi-04 <78631524+ruchi-04@users.noreply.github.com>
2023-12-21 20:48:29 +0530

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import pickle
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+diabetes_model = pickle.load(open('C:/Users/ruchi/Desktop/Multiple_disease/saved_model/diabetes_model.sav','rb'))
+heart_disease_model = pickle.load(open('C:/Users/ruchi/Desktop/Multiple_disease/saved_model/heart_disease_model.sav','rb'))
+
+with st.sidebar:
+    selected = option_menu("Multiple Disease Prediction System",
+                           
+                           ['Diabetes Prediction',
+                            'Heart Disease Prediction'],
+                           
+                           icons = ['activity','heart'],
+                           
+                           default_index = 0)
+#Diabete Prediction Page
+if(selected == "Diabetes Prediction"):
+    #page title
+    st.title('Diabetes Prediction using ML')
+    col1,col2,col3 = st.columns(3)
+    
+    with col1:
+        Pregnancies = st.text_input("Number of Pregnancies")
+    with col2:
+        Glucose = st.text_input('Glucose Level')
+    with col3:
+        BloodPressure = st.text_input('Blood Pressure value')
+    with col1:
+        SkinThickness = st.text_input('Skin Thickness value')
+    with col2:
+        Insulin = st.text_input("Insulin Level")
+    with col3:
+        BMI = st.text_input("BMI")
+    with col1:
+        DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
+    with col2:
+        Age = st.text_input('Age of the Person')
+    
+    #code for Prediction
+    diab_diagnosis= ''
+    #creating a button for Prediction
+    if st.button('Diabetes Test Result'):
+        diab_prediction = diabetes_model.predict([[Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age]])
+        if(diab_prediction[0]==1):
+            diab_diagnosis = 'The person is diabetic'
+        else:
+            diab_diagnosis = 'The person is not diabetic'
+    st.success(diab_diagnosis)
+#Heart Disease Prediction Page
+if(selected == "Heart Disease Prediction"):
+    #page title
+    st.title('Heart Disease Prediction using ML')
